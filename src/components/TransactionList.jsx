1a14d1677b216c75543099a4c9809a3dcf1a7dd4
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-06-28 21:44:00 +0530

    React changes

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -2,7 +2,7 @@ const TransactionList = () => {
   return (
     <>
       <h3>History</h3>
-      <ul id='list' classname='list'>
+      <ul classname='list'>
         <li classname='minus'>
           Cash <span>-$400</span>
           <button classname='delete-btn'>x</button>
