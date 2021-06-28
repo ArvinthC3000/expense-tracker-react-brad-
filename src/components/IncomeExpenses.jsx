be2946ff371b737bcba59c814e27f8137b18a52a
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-06-28 21:43:53 +0530

    expenses list container

--- src/components/IncomeExpenses.jsx
+++ src/components/IncomeExpenses.jsx
@@ -3,15 +3,11 @@ const IncomeExpenses = () => {
     <div class='inc-exp-container'>
       <div>
         <h4>Income</h4>
-        <p id='money-plus' class='money plus'>
-          +$0.00
-        </p>
+        <p class='money plus'>+$0.00</p>
       </div>
       <div>
         <h4>Expense</h4>
-        <p id='money-minus' class='money minus'>
-          -$0.00
-        </p>
+        <p class='money minus'>-$0.00</p>
       </div>
     </div>
   );
