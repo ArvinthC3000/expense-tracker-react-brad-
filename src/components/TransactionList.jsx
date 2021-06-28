4a62085ad473a7ce7433568dda8644773850d2b4
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-06-28 21:49:23 +0530

    Minor bugs

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -2,10 +2,10 @@ const TransactionList = () => {
   return (
     <>
       <h3>History</h3>
-      <ul classname='list'>
-        <li classname='minus'>
+      <ul className='list'>
+        <li className='minus'>
           Cash <span>-$400</span>
-          <button classname='delete-btn'>x</button>
+          <button className='delete-btn'>x</button>
         </li>
       </ul>
     </>
