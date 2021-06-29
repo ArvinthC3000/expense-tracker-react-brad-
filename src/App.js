2d1de6b352d615e021143cb0dfb0a725a8fd5111
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-06-29 22:34:58 +0530

    Provider setup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
+import React from 'react';
 import './App.css';
 import AddTransaction from './components/AddTransaction';
 import Balance from './components/Balance';
 import Header from './components/Header';
 import IncomeExpenses from './components/IncomeExpenses';
 import TransactionList from './components/TransactionList';
+import GlobalState from './context/GlobalState';
 
 const App = () => {
+  // console.log(GlobalState());
+  console.log(GlobalState);
+  console.log(<GlobalState></GlobalState>);
   return (
-    <div className='App'>
-      <Header />
-      <div className='container'>
-        <Balance />
-        <IncomeExpenses />
-        <TransactionList />
-        <AddTransaction />
-      </div>
-    </div>
+    <GlobalState>
+      <>
+        <Header />
+        <div className='container'>
+          <Balance />
+          <IncomeExpenses />
+          <TransactionList />
+          <AddTransaction />
+        </div>
+      </>
+    </GlobalState>
   );
 };
 
