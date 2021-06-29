c7a13792b60ef42f021d5824320f8d1d2527fdd7
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-06-29 22:34:45 +0530

    Global reducer setup

---
+++ src/context/AppReducer.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+const AppReducer = (state, { type, payload }) => {
+  switch (type) {
+    // case typeName:
+    //     return { ...state, ...payload }
+
+    default:
+      return state;
+  }
+};
+
+export default AppReducer;
