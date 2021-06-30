e71353aa51b47c3638919b2d8e198cfaa94514fc
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-06-30 21:56:12 +0530

    Reverting context

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,20 +1,21 @@
-import React, { useReducer } from 'react';
+import React, { createContext, useReducer } from 'react';
 import AppReducer from './AppReducer';
 import GlobalContext from './GlobalContext';
 
+// Initial state
+const initialState = {
+  transactions: [
+    { id: 1, text: 'Flower', amount: -20 },
+    { id: 2, text: 'Salary', amount: 300 },
+    { id: 3, text: 'Book', amount: -10 },
+    { id: 4, text: 'Camera', amount: 150 },
+  ],
+};
+// Create context
+const globalContext = createContext(initialState);
+
 // Provider Component
-
 const GlobalState = props => {
-  // Initial state
-  const initialState = {
-    transactions: [
-      { id: 1, text: 'Flower', amount: -20 },
-      { id: 2, text: 'Salary', amount: 300 },
-      { id: 3, text: 'Book', amount: -10 },
-      { id: 4, text: 'Camera', amount: 150 },
-    ],
-  };
-
   const [state] = useReducer(AppReducer, initialState);
 
   return (
