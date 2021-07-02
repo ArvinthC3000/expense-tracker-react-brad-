39131be6ea0ba2e1860622bea281f6a3bc81193b
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-07-02 21:16:33 +0530

    Delete transaction

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,14 +1,21 @@
 import PropTypes from 'prop-types';
+import { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 const Transaction = ({ transaction }) => {
-  const sign = transaction.amount > 0 ? '+' : '-';
+  const { deleteTransaction } = useContext(GlobalContext);
+  const { amount, text, id } = transaction;
+  const sign = amount > 0 ? '+' : '-';
+
   return (
-    <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
-      {transaction.text}{' '}
+    <li className={amount < 0 ? 'minus' : 'plus'}>
+      {text}{' '}
       <span>
-        {sign}${Math.abs(transaction.amount)}
+        {sign}${Math.abs(amount)}
       </span>
-      <button className='delete-btn'>x</button>
+      <button className='delete-btn' onClick={() => deleteTransaction(id)}>
+        x
+      </button>
     </li>
   );
 };
